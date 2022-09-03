fe4b47056b70f4c44028d01a25b3ec2113303c28
Artem Vasylyshyn <artem.kievskaia.rus@gmail.com>
2022-09-03 21:47:58 +0300

    lesson02.1

--- JsComplicated.js
+++ JsComplicated.js
@@ -3,18 +3,19 @@ const num = 266219;
 /* 2) Вывести в консоль произведение (умножение) цифр этого числа
 Например: число 123, при помощи javaScript получить каждое цифру ( 1, 2, 3 ) и перемножить их.
 Правильно использовать цикл или методы перебора. */
-const multiplyingDigitsNumber = str => {
+const Numbers = str => {
     let res = 1;
     for (let i = 0; i < str.length; i++) {
         res *= str[i];
+        console.log('1:', str);
     }
     return res;
 };
-const multiplyingDigits = multiplyingDigitsNumber(num.toString());
-console.log('2: ', multiplyingDigits);
+const newNumbers = Numbers(num.toString());
+console.log('2: ', newNumbers);
 
 // 3) Полученный результат возвести в степень 3, используя только 1 оператор (Math.pow не подходит)
-const raising = multiplyingDigits ** 3;
+const raising = newNumbers ** 3;
 console.log('3: ', raising);
 
 // 4) Вывести на экран первые 2 цифры полученного числа
